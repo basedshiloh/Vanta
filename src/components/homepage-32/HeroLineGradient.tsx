31ae4a78356a57f93ef83a81c5b3cd788a39b181
basedshiloh <0xshilloh@gmail.com>
2026-03-14 20:16:29 +0100

    Update HeroLineGradient component to modify gradient colors for enhanced visual consistency across the homepage.

--- src/components/homepage-32/HeroLineGradient.tsx
+++ src/components/homepage-32/HeroLineGradient.tsx
@@ -29,28 +29,28 @@ const HeroLineGradient = () => {
     <RevealAnimation delay={0.5}>
       <figure className="max-w-[835px] w-full mx-auto absolute top-0 left-1/2 -translate-x-1/2 h-full flex items-start justify-between -z-10">
         <div ref={lineExpandRef1} className="w-px h-0 bg-stroke-2 dark:bg-stroke-6 relative">
-          <div className="gradient-line-1 w-px h-[92px] bg-linear-[180deg,#A585FF,#1a4bd3] absolute top-0 left-0" />
+          <div className="gradient-line-1 w-px h-[92px] bg-linear-[180deg,#7e98ec,#1a4bd3] absolute top-0 left-0" />
         </div>
         <div ref={lineExpandRef2} className="w-px h-0 bg-stroke-2 dark:bg-stroke-6 relative">
-          <div className="gradient-line-2 w-px h-[92px] bg-linear-[180deg,#A585FF,#1a4bd3] absolute top-0 left-0" />
+          <div className="gradient-line-2 w-px h-[92px] bg-linear-[180deg,#7e98ec,#1a4bd3] absolute top-0 left-0" />
         </div>
         <div ref={lineExpandRef3} className="w-px h-0 bg-stroke-2 dark:bg-stroke-6 relative">
-          <div className="gradient-line-3 w-px h-[92px] bg-linear-[180deg,#A585FF,#1a4bd3] absolute top-0 left-0" />
+          <div className="gradient-line-3 w-px h-[92px] bg-linear-[180deg,#7e98ec,#1a4bd3] absolute top-0 left-0" />
         </div>
         <div ref={lineExpandRef4} className="sm:block hidden w-px h-0 bg-stroke-2 dark:bg-stroke-6 relative">
-          <div className="gradient-line-4 w-px h-[92px] bg-linear-[180deg,#A585FF,#1a4bd3] absolute top-0 left-0" />
+          <div className="gradient-line-4 w-px h-[92px] bg-linear-[180deg,#7e98ec,#1a4bd3] absolute top-0 left-0" />
         </div>
         <div ref={lineExpandRef5} className="sm:block hidden w-px h-0 bg-stroke-2 dark:bg-stroke-6 relative">
-          <div className="gradient-line-5 w-px h-[92px] bg-linear-[180deg,#A585FF,#1a4bd3] absolute top-0 left-0" />
+          <div className="gradient-line-5 w-px h-[92px] bg-linear-[180deg,#7e98ec,#1a4bd3] absolute top-0 left-0" />
         </div>
         <div ref={lineExpandRef6} className="lg:block hidden w-px h-0 bg-stroke-2 dark:bg-stroke-6 relative">
-          <div className="gradient-line-6 w-px h-[92px] bg-linear-[180deg,#A585FF,#1a4bd3] absolute top-0 left-0" />
+          <div className="gradient-line-6 w-px h-[92px] bg-linear-[180deg,#7e98ec,#1a4bd3] absolute top-0 left-0" />
         </div>
         <div ref={lineExpandRef7} className="lg:block hidden w-px h-0 bg-stroke-2 dark:bg-stroke-6 relative">
-          <div className="gradient-line-7 w-px h-[92px] bg-linear-[180deg,#A585FF,#1a4bd3] absolute top-0 left-0" />
+          <div className="gradient-line-7 w-px h-[92px] bg-linear-[180deg,#7e98ec,#1a4bd3] absolute top-0 left-0" />
         </div>
         <div ref={lineExpandRef8} className="lg:block hidden w-px h-0 bg-stroke-2 dark:bg-stroke-6 relative">
-          <div className="gradient-line-8 w-px h-[92px] bg-linear-[180deg,#A585FF,#1a4bd3] absolute top-0 left-0" />
+          <div className="gradient-line-8 w-px h-[92px] bg-linear-[180deg,#7e98ec,#1a4bd3] absolute top-0 left-0" />
         </div>
       </figure>
     </RevealAnimation>
