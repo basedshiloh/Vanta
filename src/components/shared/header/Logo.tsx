ef65f3eafdb6c8baf3f65c71071582a877cc7487
basedshiloh <0xshilloh@gmail.com>
2026-03-14 20:05:24 +0100

    Enhance Logo component to include mobile versions of Vanta logos for better responsiveness and branding consistency across devices.

--- src/components/shared/header/Logo.tsx
+++ src/components/shared/header/Logo.tsx
@@ -1,5 +1,7 @@
 import vantaDarkLogo from '@public/images/shared/vanta-dark-mode.svg';
+import vantaDarkLogoMobile from '@public/images/shared/vanta-dark-logo.svg';
 import vantaLightLogo from '@public/images/shared/vanta-light-mode.svg';
+import vantaLightLogoMobile from '@public/images/shared/vanta-light-logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -14,9 +16,9 @@ const Logo = () => {
         </figure>
 
         {/* mobile logo */}
-        <figure className="block max-w-[44px] lg:hidden">
-          <Image src={vantaLightLogo} alt="Vanta Trading Bot" className="block w-full dark:hidden" />
-          <Image src={vantaDarkLogo} alt="Vanta Trading Bot" className="hidden w-full dark:block" />
+        <figure className="block max-w-[120px] lg:hidden">
+          <Image src={vantaLightLogoMobile} alt="Vanta Trading Bot" className="block w-full dark:hidden" />
+          <Image src={vantaDarkLogoMobile} alt="Vanta Trading Bot" className="hidden w-full dark:block" />
         </figure>
       </Link>
     </div>
