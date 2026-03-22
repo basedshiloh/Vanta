f044871b0e2354f3014369c22aa0041c4228bf1b
basedshiloh <0xshilloh@gmail.com>
2026-03-22 20:13:01 +0100

    Enhance NavCTAButton component by extending props to include 'target' and 'rel' attributes for improved link functionality and flexibility.

--- src/components/shared/header/NavCTAButton.tsx
+++ src/components/shared/header/NavCTAButton.tsx
@@ -1,17 +1,26 @@
 import LinkButton from '@/components/ui/button/LinkButton';
 import { cn } from '@/utils/cn';
+import { AnchorHTMLAttributes } from 'react';
 
-interface NavCTAButtonProps {
+interface NavCTAButtonProps
+  extends Pick<AnchorHTMLAttributes<HTMLAnchorElement>, 'target' | 'rel'> {
   btnClassName?: string;
   href: string;
   label: string;
   className?: string;
 }
 
-const NavCTAButton = ({ btnClassName, href = '/', label, className }: NavCTAButtonProps) => {
+const NavCTAButton = ({
+  btnClassName,
+  href = '/',
+  label,
+  className,
+  target,
+  rel,
+}: NavCTAButtonProps) => {
   return (
     <div className={cn('hidden items-center justify-center xl:flex', className)}>
-      <LinkButton href={href} className={cn('btn btn-md', btnClassName)}>
+      <LinkButton href={href} className={cn('btn btn-md', btnClassName)} target={target} rel={rel}>
         {label}
       </LinkButton>
     </div>
